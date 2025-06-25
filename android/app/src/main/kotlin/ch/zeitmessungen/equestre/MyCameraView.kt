73171b26038ccae398b33b29404420d0c5b50caa
mg3994 <manishgautammg7@yahoo.com>
2025-06-25 09:05:52 +0530

    test build 02

--- android/app/src/main/kotlin/ch/zeitmessungen/equestre/MyCameraView.kt
+++ android/app/src/main/kotlin/ch/zeitmessungen/equestre/MyCameraView.kt
@@ -30,6 +30,7 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.platform.PlatformView
+import androidx.media3.effect.TextureOverlay
 
 @UnstableApi
 class MyCameraView(
